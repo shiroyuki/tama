850d17ea2d8a7d7fe3c6c0f84382ed0d9bb945dc
Juti Noppornpitak <jnopporn@shiroyuki.com>
2014-11-27 21:42:39 -0500

    Added the missing files

---
+++ static/js/common/trpc.js
@@ -0,0 +1,42 @@
+function ToriRpcClient(url) {
+    this.WebSocketClient(url);
+};
+
+$.extend(ToriRpcClient.prototype, WebSocketClient.prototype, {
+    request: function (service, method, data) {
+        if (!this.connected()) {
+            this.connect();
+        }
+
+        this.send({
+            id: this.getRequestType(service, method),
+            service: service,
+            method: method,
+            data: data
+        });
+    },
+
+    onMessage: function (event) {
+        var data = this.extractDataFromEvent(event),
+            type = 'message'
+        ;
+
+        if (data !== null) {
+            type = this.getRequestType(data.service, data.method);
+        }
+
+        console.log('On Message:', type, ':', data);
+
+        this.handleEvent(type, data);
+    },
+
+    getRequestType: function (service, method) {
+        if (service) {
+            return [service, method].join('.');
+        } else if (method) {
+            return method;
+        }
+
+        return 'message';
+    }
+});
---
+++ static/js/common/ws.js
@@ -0,0 +1,82 @@
+function WebSocketClient(url) {
+    this.WebSocketClient(url);
+};
+
+$.extend(WebSocketClient.prototype, {
+    WebSocketClient: function (url) {
+        this._connected = false;
+        this.url    = url;
+        this.client = null;
+        this.handlerMap = {
+            open:    [],
+            message: [],
+            close:   []
+        };
+    },
+
+    connected: function () {
+        return this._connected;
+    },
+
+    reset: function () {
+        this.client = null;
+    },
+
+    init: function () {
+        this.client = new WebSocket(this.url);
+        this.client.addEventListener('open', $.proxy(this.onOpen, this));
+        this.client.addEventListener('close', $.proxy(this.onClose, this));
+        this.client.addEventListener('message', $.proxy(this.onMessage, this));
+    },
+
+    connect: function () {
+        if (this.client !== null) {
+            return;
+        }
+
+        this.init();
+    },
+    
+    on: function (eventType, eventHandler) {
+        if (this.handlerMap[eventType] === undefined) {
+            this.handlerMap[eventType] = [];
+        }
+
+        this.handlerMap[eventType].push(eventHandler);
+    },
+
+    send: function (message) {
+        this.client.send(JSON.stringify(message));
+    },
+    
+    handleEvent: function (type, data) {
+        var handlers,
+            i
+        ;
+
+        handlers = this.handlerMap[type];
+
+        for (i in handlers) {
+            handlers[i](data);
+        }
+    },
+
+    extractDataFromEvent: function (event) {
+        return event.data !== undefined ? JSON.parse(event.data) : null;
+    },
+
+    onMessage: function (event) {
+        var data = this.extractDataFromEvent(event);
+        this.handleEvent('message', data);
+    },
+
+    onOpen: function (event) {
+        this._connected = true;
+        this.handleEvent('open', event);
+    },
+
+    onClose: function (event) {
+        this._connected = false;
+        this.handleEvent('close', event);
+    }
+});
